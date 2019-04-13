eb3dfc913d07333af01c35460c93d3cef7a1e7de
Inderjeetkaur36 <waytoinderjeetkaur36@gmail.com>
2019-04-12 17:36:00 -0700

    BluePop

---
+++ app/src/main/java/com/example/bluepop/ui/PaymentActivity.java
@@ -0,0 +1,21 @@
+package com.example.bluepop.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bluepop.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+
+
+}
+
+
+
